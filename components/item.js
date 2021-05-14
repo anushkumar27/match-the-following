e52d79a5691d0f9367a8a43873920999003c6e75
Anush Kumar <anushkumar27@gmail.com>
2021-05-15 01:04:09 +0530

    Formatted Files

--- components/item.js
+++ components/item.js
@@ -4,15 +4,15 @@ import { ArcherElement } from "react-archer";
 import { ItemType } from "../constants/itemTypes";
 
 const boxStyle = {
-  padding: "10px",
-  marginTop: "15px",
-  marginBottom: "15px",
-  border: "1px solid black",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-	cursor: "pointer"
+	padding: "10px",
+	marginTop: "15px",
+	marginBottom: "15px",
+	border: "1px solid black",
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
+	alignItems: "center",
+	cursor: "pointer",
 };
 
 /**
@@ -20,78 +20,86 @@ const boxStyle = {
  * It created all the ArcherElements and populates all the properties. It receives the relations from the MatchTheFollowingContainer.
  */
 export default function Item({
-  itemDetails,
-  isLHS,
-  onClickFunc,
-  arrowDestination,
+	itemDetails,
+	isLHS,
+	onClickFunc,
+	arrowDestination,
 }) {
-  /**
-   * This method transforms array of destination ids into JSON array of relations required for ArcherElement component.
-   *
-   * @param {Array of detination element ids} destArray
-   * @returns ArcherElement: Props: relations
-   */
-  const getRelations = (destArray) => {
-    let relations = [];
-    if (destArray != undefined) {
-      destArray.forEach(function (dest, index) {
-        relations = [
-          ...relations,
-          {
-            targetId: dest,
-            targetAnchor: "left",
-            sourceAnchor: "right",
-          },
-        ];
-      });
-    }
+	/**
+	 * This method transforms array of destination ids into JSON array of relations required for ArcherElement component.
+	 *
+	 * @param {Array of detination element ids} destArray
+	 * @returns ArcherElement: Props: relations
+	 */
+	const getRelations = (destArray) => {
+		let relations = [];
+		if (destArray != undefined) {
+			destArray.forEach(function (dest, index) {
+				relations = [
+					...relations,
+					{
+						targetId: dest,
+						targetAnchor: "left",
+						sourceAnchor: "right",
+					},
+				];
+			});
+		}
 
-    return relations;
-  };
+		return relations;
+	};
 
-  /**
-   * This method handles rendering DOM for different types for items
-   *
-   * @returns item DOM based on the type of item
-   */
-  const getItemDOM = () => {
-    switch (itemDetails.type) {
-      case ItemType.TEXT:
-        return itemDetails.text;
-      case ItemType.IMAGE:
-        if (itemDetails.imgSource) {
-          return (
-            <Image
-              src={itemDetails.imgSource}
-              alt="Unable to Load Image"
-              width={50}
-              height={50}
-            />
-          );
-        } else {
-          return "Unable to Load Image";
-        }
-    }
-  };
+	/**
+	 * This method handles rendering DOM for different types for items
+	 *
+	 * @returns item DOM based on the type of item
+	 */
+	const getItemDOM = () => {
+		switch (itemDetails.type) {
+			case ItemType.TEXT:
+				return itemDetails.text;
+			case ItemType.IMAGE:
+				if (itemDetails.imgSource) {
+					return (
+						<Image
+							src={itemDetails.imgSource}
+							alt="Unable to Load Image"
+							width={50}
+							height={50}
+						/>
+					);
+				} else {
+					return "Unable to Load Image";
+				}
+		}
+	};
 
-  if (isLHS) {
-    return (
-      <ArcherElement
-        id={itemDetails.id}
-        relations={getRelations(arrowDestination)}
-      >
-        <div id={itemDetails.id} style={boxStyle} onClick={() => onClickFunc(itemDetails.id)}>
-          {getItemDOM()}
-        </div>
-      </ArcherElement>
-    );
-  } else {
-    return (
-      <ArcherElement id={itemDetails.id}>
-        <div id={itemDetails.id} onClick={() => onClickFunc(itemDetails.id)} style={boxStyle}>
-          {getItemDOM()}
-        </div>
-      </ArcherElement>
-    );
-  }
+	if (isLHS) {
+		return (
+			<ArcherElement
+				id={itemDetails.id}
+				relations={getRelations(arrowDestination)}
+			>
+				<div
+					id={itemDetails.id}
+					style={boxStyle}
+					onClick={() => onClickFunc(itemDetails.id)}
+				>
+					{getItemDOM()}
+				</div>
+			</ArcherElement>
+		);
+	} else {
+		return (
+			<ArcherElement id={itemDetails.id}>
+				<div
+					id={itemDetails.id}
+					onClick={() => onClickFunc(itemDetails.id)}
+					style={boxStyle}
+				>
+					{getItemDOM()}
+				</div>
+			</ArcherElement>
+		);
+	}
 }
--- pages/example.js
+++ pages/example.js
@@ -1,11 +1,11 @@
 /**
  * Assumption:
  * LHS and RHS list do not change at runtime
- * 
+ *
  * State Variables:
  * 1. To keep track of current selection
  * 2. LHS Individual Item  = [ destination ids ]
- * 
+ *
  * Components:
  * 1. Main Layout: Stacks: LHS and RHS
  * 2. Individual Lists Items Containers
@@ -19,89 +19,92 @@
  * 2. Check Sanity of the input: LHS, RHS
  */
 
-import MatchTheFollowingContainer from '../components/matchTheFollowingContainer';
+import MatchTheFollowingContainer from "../components/matchTheFollowingContainer";
 import { ItemType } from "../constants/itemTypes";
 
-const LHS = [{
-	"id": "111",
-	"type": ItemType.TEXT,
-	"text": "Tiger"
-},
-{
-	"id": "222",
-	"type": ItemType.TEXT,
-	"text": "Lion"
-},
-{
-	"id": "333",
-	"type": ItemType.TEXT,
-	"text": "Elephant"
-},
-{
-	"id": "444",
-	"type": ItemType.TEXT,
-	"text": "Zebra"
-},
-{
-	"id": "555",
-	"type": ItemType.TEXT,
-	"text": "Monkey"
-}]
+const LHS = [
+	{
+		id: "111",
+		type: ItemType.TEXT,
+		text: "Tiger",
+	},
+	{
+		id: "222",
+		type: ItemType.TEXT,
+		text: "Lion",
+	},
+	{
+		id: "333",
+		type: ItemType.TEXT,
+		text: "Elephant",
+	},
+	{
+		id: "444",
+		type: ItemType.TEXT,
+		text: "Zebra",
+	},
+	{
+		id: "555",
+		type: ItemType.TEXT,
+		text: "Monkey",
+	},
+];
 
-const RHS123 = [{
-	"id": "aaa",
-	"type": ItemType.TEXT,
-	"text": "Tiger"
-},
-{
-	"id": "bbb",
-	"type": ItemType.TEXT,
-	"text": "Lion"
-},
-{
-	"id": "ccc",
-	"type": ItemType.TEXT,
-	"text": "Elephant"
-},
-{
-	"id": "ddd",
-	"type": ItemType.TEXT,
-	"text": "Zebra"
-},
-{
-	"id": "fff",
-	"type": ItemType.TEXT,
-	"text": "Monkey"
-}]
+const RHS123 = [
+	{
+		id: "aaa",
+		type: ItemType.TEXT,
+		text: "Tiger",
+	},
+	{
+		id: "bbb",
+		type: ItemType.TEXT,
+		text: "Lion",
+	},
+	{
+		id: "ccc",
+		type: ItemType.TEXT,
+		text: "Elephant",
+	},
+	{
+		id: "ddd",
+		type: ItemType.TEXT,
+		text: "Zebra",
+	},
+	{
+		id: "fff",
+		type: ItemType.TEXT,
+		text: "Monkey",
+	},
+];
 
-const RHS = [{
-	"id": "aaa",
-	"type": ItemType.IMAGE,
-	"imgSource": "https://images.freeimages.com/images/large-previews/66d/tiger-1388270.jpg",
-	
-},
-{
-	"id": "bbb",
-	"type": ItemType.IMAGE,
-	"imgSource": "https://images.freeimages.com/images/large-previews/3e1/elephant-1361859.jpg",
-},
-{
-	"id": "ccc",
-	"type": ItemType.IMAGE,
-	"imgSource": "https://images.freeimages.com/images/large-previews/6c9/lion-1402323.jpg",
-},
-{
-	"id": "ddd",
-	"type": ItemType.IMAGE,
-	"imgSource": "https://images.freeimages.com/images/large-previews/f41/zebra-1393530.jpg",
-},
-]
+const RHS = [
+	{
+		id: "aaa",
+		type: ItemType.IMAGE,
+		imgSource:
+			"https://images.freeimages.com/images/large-previews/66d/tiger-1388270.jpg",
+	},
+	{
+		id: "bbb",
+		type: ItemType.IMAGE,
+		imgSource:
+			"https://images.freeimages.com/images/large-previews/3e1/elephant-1361859.jpg",
+	},
+	{
+		id: "ccc",
+		type: ItemType.IMAGE,
+		imgSource:
+			"https://images.freeimages.com/images/large-previews/6c9/lion-1402323.jpg",
+	},
+	{
+		id: "ddd",
+		type: ItemType.IMAGE,
+		imgSource:
+			"https://images.freeimages.com/images/large-previews/f41/zebra-1393530.jpg",
+	},
+];
 
 export default function Example() {
-	return (
-		<MatchTheFollowingContainer 
-			LHS={LHS}
-			RHS={RHS}
-		/>
-	)
+	return <MatchTheFollowingContainer LHS={LHS} RHS={RHS} />;
 }
